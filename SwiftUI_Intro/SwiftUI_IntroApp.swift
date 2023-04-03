3916c1a74f2d8199ab26d1551c4f7685f38abdb7
friddler <yhma22.nilsfr@folkuniversitetet.nu>
2023-04-03 11:09:54 +0200

    Initial Commit

---
+++ SwiftUI_Intro/SwiftUI_IntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_IntroApp.swift
+//  SwiftUI_Intro
+//
+//  Created by Frida Nilsson on 2023-04-03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_IntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
